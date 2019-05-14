eb028aa5c7e7c04b5d63aeef16dc333b82a6bbd7
Johnscar44 <50434203+Johnscar44@users.noreply.github.com>
2019-05-14 14:07:51 -0400

    Update game2.py

--- game2.py
+++ game2.py
@@ -1,21 +1,21 @@
 command = ""
 while True:
     command = input('< ').lower()
-    if command = "start":
+    if command == "start":
         print('car has started')
-    elif command = "stop":
+    elif command == "stop":
         print('car has stopped')
-    elif command = "go":
+    elif command == "go":
         print('we are moving the fuck out')
-    elif command = "help":
+    elif command == "help":
         print("""
 start - to start the car
 go - move the fuck out
 stop -stop the car
 quit - ditch this hot car and bail
     """)
-    elif command = "quit":
+    elif command == "quit":
         print('ditch this hot car and bail bro')
         break
     else:
-    print('i dont know what the fuck your saying dude?')
+        print('i dont know what the fuck your saying dude?')
